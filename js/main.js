40c1c1b17a9dca8dca87d7f1cbc53189ee8c7d8b
ljluiscode <91440555+ljluiscode@users.noreply.github.com>
2021-10-17 11:35:34 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -30,13 +30,13 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 //Accordion skills
-const skillsContent = document.getElementsByClassName('skills__content')
-    skillsHeader = document.querySelectorAll('.skills__header')
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
 
 function toggleSkills(){
     let itemClass = this.parentNode.className
 
-    for(i=0; i<skillsContent.length; i++){
+    for(i = 0; i < skillsContent.length; i++){
         skillsContent[i].className = 'skills__content skills__close'
     }
     if(itemClass === 'skills__content skills__close'){
